37379e5cdadc7ed38908f72b7e66e8c660f2e8de
Matthew Planchard <msplanchard@gmail.com>
2017-07-25 20:27:55 -0500

    Setup readme, moved .gitignore, package.json & setup.py

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Challenges
 Coding challenges
+
+## Setup
+
+### Python
+
+* Don't forget to set up a virtual environment for Python
+when getting started, and to activate it each time you're
+working on this project:
+  * `python3.6 -m venv venv --copies`
+  * `source venv/bin/activate`
+* If you're using PyCharm or IntelliJ, be sure to set the
+virtual environment's Python interpreter as the default in
+settings -> project: Challenges -> project interpreter
+* Install the project in development mode:
+  * `pip install -e .`
+* You can update the project requirements at any time by running
+  * `pip install -e . --upgrade` OR
+  * `pip install -e . -U`
+
+### Node
+
+* run `npm install` from the root directory
+* if third party packages are added to the `package.json`
+requirements file, you acn update your environment by running
+`npm install` again
+
+## Layout
+
+This project is currently organized by year and month. Within
+each month directory, challenges are numbered, with a lightly
+descriptive name. Challenges are either designed to be completed
+using Python or JavaScript. Which is to be used should be obvious
+based on the file extension.
+
+## Running files
+
+### Python
+
+* Ensure you have activated your virtual environment `source venv/bin/activate`
+* Run `python <your_script_path>`
+
+### JavaScript
+
+* Run `node <your_script_path>`
---
+++ setup.py
@@ -0,0 +1,64 @@
+"""
+Simple setup for challenges
+"""
+
+from os.path import dirname, join, realpath
+from setuptools import setup, find_packages
+
+
+NAME = 'challenges'
+URL = 'https://github.com/madisonhicks/Challenges'
+AUTHOR = 'Madison Hicks & Matthew Planchard'
+EMAIL = 'madison.carlisle.hicks@gmail.com'
+
+SHORT_DESC = 'fun coding challenges'
+LONG_DESC = SHORT_DESC
+KEYWORDS = []
+
+PACKAGE_DEPENDENCIES = [
+    'pytest',
+]
+SETUP_DEPENDENCIES = []
+TEST_DEPENDENCIES = []
+EXTRAS_DEPENDENCIES = {}
+
+ENTRY_POINTS = {}
+
+
+# See https://pypi.python.org/pypi?%3Aaction=list_classifiers for all
+# available setup classifiers
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    # 'Development Status :: 5 - Production/Stable',
+    # 'Environment :: Web Environment',
+    'Intended Audience :: Developers'
+    # 'License :: Other/Proprietary License',
+    # 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3 :: Only',
+    'Programming Language :: Python :: 3.6'
+]
+
+
+__version__ = '0.1.0'
+
+
+setup(
+    name=NAME,
+    version=__version__,
+    description=SHORT_DESC,
+    long_description=LONG_DESC,
+    url=URL,
+    author=AUTHOR,
+    author_email=EMAIL,
+    classifiers=CLASSIFIERS,
+    keywords=KEYWORDS,
+    packages=find_packages(exclude=['*.tests', '*.tests.*']),
+    install_requires=PACKAGE_DEPENDENCIES,
+    setup_requires=SETUP_DEPENDENCIES,
+    tests_require=TEST_DEPENDENCIES,
+    extras_require=EXTRAS_DEPENDENCIES,
+)
